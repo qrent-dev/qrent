edc4bcbaaee0419ac200171157b1e4121fe749fc
fisher-byte <yyzyfish5@gmail.com>
2025-06-09 20:46:44 +1000

    feat: adjust Dify chatbot button size to 1.5x and add defer attribute

--- packages/frontend/src/app/[locale]/layout.tsx
+++ packages/frontend/src/app/[locale]/layout.tsx
@@ -64,6 +64,7 @@ export default async function LocaleLayout({
           src="https://udify.app/embed.min.js"
           id="Pk5JLyWtauKU4b1H"
           strategy="afterInteractive"
+          defer
         />
         {/* Dify Chatbot Custom Styles */}
         <style
@@ -71,9 +72,7 @@ export default async function LocaleLayout({
             __html: `
             #dify-chatbot-bubble-button {
               background-color: #1C64F2 !important;
-              transform: scale(1.6) !important;
-              bottom: 30px !important;
-              right: 30px !important;
+              transform: scale(1.5) !important;
             }
             #dify-chatbot-bubble-window {
               width: 24rem !important;
