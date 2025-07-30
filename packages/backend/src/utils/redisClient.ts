17570eadf1d1a2e8e26a60a0ac90d5b88e5ec228
wiperi <solidtreepassing@gmail.com>
2025-07-30 14:25:31 +1000

    feat: add REDIS_URL to environment configuration

--- packages/backend/src/utils/redisClient.ts
+++ packages/backend/src/utils/redisClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const redis = createClient({
-  url: process.env.REDIS_URL || `redis://localhost:${process.env.REDIS_PORT || 6379}`,
+  url: process.env.REDIS_URL,
 });
 
 redis.on('error', err => console.log('Redis Client Error', err));
